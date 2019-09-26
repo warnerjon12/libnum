b22e912ad35f94699a37e7243e9dc1f8de692765
Jonathan Warner <warnerjon12@gmail.com>
2019-09-25 23:07:05 -0600

    Bug fixes in compat

--- libnum/compat.py
+++ libnum/compat.py
@@ -1,18 +1,21 @@
 import math
 
 try:
-    import __builtins__
+    builtins = __builtins__
+except NameError:
+    import builtins
+    builtins = builtins
+
+try:
     basestring = basestring
     xrange = xrange
     long = long
-    builtins = __builtins__
 
     def math_log2(x):
         return math.log(x) / math.log(2)
+
 except NameError:
-    import builtins
     basestring = str
     xrange = range
     long = int
-    builtins = builtins
     math_log2 = math.log2
